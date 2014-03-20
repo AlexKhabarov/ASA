095327a0d729b4bd4b697a23b061e644d31c4cea
Alex Khabarov <alexkhabarov@Alexs-MacBook-Pro.local>
2014-03-20 21:34:38 +0400

    Code relation metalanguage; Relation retriever; Boost::Assign; Minor Bug Fixes

--- SSGL/main.cpp
+++ SSGL/main.cpp
@@ -2,35 +2,30 @@
 
 #include "../../SWL/SWL/$.h"
 
-// Nodes Dumper
-// if (auto fs = $sl$node(ForStmt)) fs->dump()
-
 $main
 {
     using namespace ssgl;
     
-    analysis::builder{$argc, $argv}
-        $sl$analyzer
-        (
-             declStmt(),
-             outs() << "declaration\n"
-        )
-        $sl$analyzer
-        (
-             forStmt(hasLoopInit(declStmt(hasSingleDecl(varDecl(hasInitializer(integerLiteral(equals(0)))))))) $sl$bind(ForStmt),
-             outs() << "got for (int i = 0; ...; ...) ... " << $sl$node(ForStmt) << "\n"
-        )
-        $sl$analyzer
-        (
-             breakStmt(),
-             outs() << "got break statement\n"
+#define $sl$relation(cell, begin, end) \
+    (#cell, relation_retriever::relation{node begin, node end, $builder.retriever_.source_manager_})
+#define $sl$relation_s(begin_name, end_name) \
+    ( \
+        #begin_name, \
+        relation_retriever::relation{node $sl$loc(begin_name), node $sl$loc(end_name), $builder.retriever_.source_manager_} \
+    )
+    
+#define $sl$analyzer(name, type, ...) $sl$analyzer$basic(name()$sl$bind(type), if (auto node = $sl$node(type)) insert($builder.retriever_[#type]) __VA_ARGS__)
+    
+    auto$exr $builder = analysis::builder{$argc, $argv};
+    $builder
+        $sl$analyzer(forStmt, ForStmt,
+            $sl$relation(Start,  $sl$loc$start,          $sl$loc(LParen))
+            $sl$relation(LParen, $sl$loc(LParen),        ->getInit()$sl$loc$start)
+            $sl$relation(Init,   ->getInit()$sl$loc$end, ->getCond()$sl$loc$start)
         )
     ();
+    
 }
-
-// declStmt = int@varName@=@expr@;
-// forStmt  = for@(@declStmt...@;@exprStmt@;@exprStmt@)@{@stmts@}
-// ifStmt   = if@(@exprStmt@)@{@stmts@}
 
 // Automatically generated undef includes: 
 #include "/Users/alexkhabarov/Dropbox/Programming/Xcode/Workspaces/ASA/Projects/SSGL/SSGL/../../SWL/SWL/undef.h"
