0e2084bb2f6c09275dfb04ae0061149277b20a2b
Alex Khabarov <alexkhabarov@Alexs-MacBook-Pro.local>
2014-03-20 21:35:05 +0400

    (null)

--- SSGL/analysis_builder.h
+++ SSGL/analysis_builder.h
@@ -6,10 +6,11 @@
 //  Copyright (c) 2014 Heaven Inc. All rights reserved.
 //
 
-#ifndef SG_AnalysisBuilder_h
-#define SG_AnalysisBuilder_h
+#ifndef SGGL_analysis_builder_h
+#define SGGL_analysis_builder_h
 
 #include "analyzer.h"
+#include "relation_retriever.h"
 #include "../../SWL/SWL/$.h"
 
 SSGL_BEGIN
@@ -18,21 +19,29 @@ namespace analysis {
 
 $obj(builder, MatchFinder, base_i)
 $interface
+    CompilerInstance    compiler_;
+    CommonOptionsParser options_parser_;
+    ClangTool           tool_;
+    
+    relation_retriever  retriever_;
+    
     builder(int$ex argc, char const** argv) :
+        compiler_      {},
         options_parser_{argc, argv},
         tool_          {options_parser_.getCompilations(),
-                        options_parser_.getSourcePathList()}
+                        options_parser_.getSourcePathList()},
+        retriever_     {({
+                        compiler_.createDiagnostics();
+                        compiler_.createSourceManager(tool_.getFiles());
+                        ref(compiler_.getSourceManager());
+                       })}
     {}
-
+    
     $def  (addMatcher(a.matcher_, &a.printer_);       $self$ex, add_analyzer, $ex  a)
     $def_s(tool_.run(newFrontendActionFactory(this)); $self$ex, run)
     $def_s(run(),                                               operator())
     $def  (add_analyzer(a),                                     operator(),   $exr a)
     $def  (add_analyzer(a),                                     operator(),   $ex  a)
-    
-$internal
-    CommonOptionsParser options_parser_;
-    ClangTool           tool_;
 $end
 
 } // namespace analysis
